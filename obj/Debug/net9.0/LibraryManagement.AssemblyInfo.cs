54cc6c31dace6038b3ccb6deda09f11efd8f8741
Ufuk ABRAVACI <108156229+ufukabravaci@users.noreply.github.com>
2025-04-21 22:36:55 +0300

    Readme Screenshots

--- obj/Debug/net9.0/LibraryManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3923f26addbe9731b21b09206fa937186a87c4be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ebd12a81d695c88db31734a56c63f4b922a9b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
