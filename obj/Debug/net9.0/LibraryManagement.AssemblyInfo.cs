8a03f7f643366d8a124352276912cf9a3dcc7ee5
Ufuk ABRAVACI <108156229+ufukabravaci@users.noreply.github.com>
2025-04-26 11:16:33 +0300

    .Bak file added

--- obj/Debug/net9.0/LibraryManagement.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70e0f4af015e13ad9e171fc9efca05ee4c7c791f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32fd5c45ccc0f1bcf49563dba111987246499afc")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
